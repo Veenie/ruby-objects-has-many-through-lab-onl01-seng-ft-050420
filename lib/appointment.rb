3ab9be81d58e8b8cab8a8bafe57de11fdd717128
Raveen K Sharma <raveen08@gmail.com>
2020-05-21 00:56:44 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,11 @@
 class Appointment
-  attr_accessor :name
+  attr_accessor :date, :patient, :doctor
   @@all = []
   
-  def initialize(name)
-    @name = name
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
     @@all << self
   end
   
